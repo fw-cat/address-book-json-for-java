f58508beb83c1b6d88b44eb7c9879325d4d45981
miyahara <kenmiya.sns@gmail.com>
2024-01-10 14:43:52 +0900

    [refactor]不要importの削除

--- jp/ac/ohara/AddressBookManager.java
+++ jp/ac/ohara/AddressBookManager.java
@@ -3,7 +3,6 @@ package jp.ac.ohara;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
